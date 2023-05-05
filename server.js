548527a6400caa3fd6a6e5649a440b9015936a6d
Fan Yiwei <nf140901@gmail.com>
2023-05-05 14:30:59 +0800

    solve cross-orgin error using mw

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require("express")
 const axios = require("axios")
 const app = express()
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
+
 app.get("/api/getnewgoods", function (req, res) {
   res.json({
     products: [
